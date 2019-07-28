ac55ba56178f9e206dcc46eab753a305bf0947ea
KHR1570PH3R <khr1570ph3r@yahoo.com>
2019-07-28 18:41:44 -0400

    Froms Atom :D

--- testProgram.c
+++ testProgram.c
@@ -4,10 +4,6 @@ int main(int argc, char const *argv[]) {
   printf("%s\n", "Hello World");
   printf("%s\n","Bye");
   printf("%s\n", "My name's Khristopher");
+  printf("%s\n", "Desde Atom xD");
   return 0;
-  hola
-  asdasdasd
-
-  asdas
-  git bahs
 }
